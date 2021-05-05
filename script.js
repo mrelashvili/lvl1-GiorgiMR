eeb10bfec17d5aa89751eecd5f6d77f0331086e9
Giorgi Mrelashvili <mrmrela5@gmail.com>
2021-05-05 17:43:52 +0400

    update-v1

--- script.js
+++ script.js
@@ -1,218 +1,43 @@
-"use strict";
+'use strict';
 
-//Selecting elements
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+// const inpValue = document.querySelector('.inp_txt').value;
+// const clck = document.querySelector('.clck-btn');
 
-const data = document.querySelector(".new-data");
-const avgHtml = document.querySelector(".avg-html");
-const avgCss = document.querySelector(".avg-css");
-const avgJs = document.querySelector(".avg-js");
-const html = document.querySelector(".html");
-const head = document.querySelector(".head-data");
-const btn = document.querySelector(".btn");
+///////////////////////////////////////
 
-const subBtn = document.querySelector(".sb-btn");
-const subInpt = document.getElementById("sbj_inp");
+const getSong = function (data) {
+  const html = `<article class="display">
+          <img class="song__img" src="${data.results[1].artworkUrl100}" />
+          <div class="song__data">
+            <h3 class="song__name">${data.results[1].trackName}</h3>
+            <h4 class="artist__name">${data.results[1].artistName}</h4>
 
-const name_inp = document.getElementById("name_inp");
-const surname = document.getElementById("surn_inp");
-const css_inp = document.getElementById("css_inp");
-const html_inp = document.getElementById("html");
-const js = document.getElementById("js");
+            <p class="row"><span>🆔 </span>${data.results[1].artistId}</p>
+            <p class="row"><span>💰</span>${data.results[1].collectionPrice} $</p>
+            <p class="row"><span> ⬇</span><a href="${data.results[1].trackViewUrl}">Click For More</a></p>
+          </div>
+          <h4 class="preview"><audio controls>
+  <source src="${data.results[1].previewUrl}" type="audio/mpeg">
+</audio></h4>
+        </article>`;
 
-///USERS.
-const users = [
-  {
-    name: "Ricky",
-    surname: "Martin",
-    points: [83, 75, 96, 55],
-  },
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
-  {
-    name: "David",
-    surname: "Benioff",
-    points: [74, 87, 69],
-  },
+// getSong('Rihanna');
 
-  {
-    name: "George",
-    surname: "Martin",
-    points: [87, 93, 91],
-  },
-
-  {
-    name: "Daniel",
-    surname: "Weiss",
-    points: [56, 83, 75],
-  },
-
-  {
-    name: "Walter",
-    surname: "White",
-    points: [52, 68, 86],
-  },
-
-  {
-    name: "Jesse",
-    surname: "Pinkman",
-    points: [67, 51, 70],
-  },
-];
-
-const subjects = ["Html Point", "CSS Point", "Javascript Point"];
-
-class fullTable {
-  users = [];
-  subjects = [];
-  htmlAverage =
-    users.map((user) => user.points[0]).reduce((acc, point) => acc + point, 0) /
-    users.length;
-
-  cssAverage =
-    users.map((user) => user.points[1]).reduce((acc, point) => acc + point, 0) /
-    users.length;
-
-  javascriptAverage =
-    users
-      .map((user) => user.points[2])
-      .reduce((acc, point) => Math.floor(acc + point, 0)) / users.length;
-
-  constructor(user, subjects) {
-    this.user = user;
-    this.subjects = subjects;
-    this.StudentEventListener();
-    this.subEventListener();
-    this.displayData();
-    this.headField();
-    this.htmlAverage =
-      users
-        .map((user) => user.points[0])
-        .reduce((acc, point) => acc + point, 0) / users.length;
-    this.cssAverage =
-      users
-        .map((user) => user.points[1])
-        .reduce((acc, point) => acc + point, 0) / users.length;
-
-    this.javascriptAverage =
-      users
-        .map((user) => user.points[2])
-        .reduce((acc, point) => Math.floor(acc + point, 0)) / users.length;
-    this.avgUpdt();
-  }
-
-  avgUpdt() {
-    avgHtml.textContent = this.htmlAverage.toFixed(2);
-    avgCss.textContent = this.cssAverage.toFixed(2);
-    avgJs.textContent = this.javascriptAverage.toFixed(2);
-  }
-
-  headField() {
-    //head field.
-    let sub = "";
-    for (let i = 0; i < this.subjects.length; i++) {
-      sub += `<td>${this.subjects[i]}</td>`;
-    }
-    head.innerHTML += sub;
-  }
-
-  displayData() {
-    let self = this;
-    //display datas..
-
-    users.forEach(function (user, index) {
-      data.innerHTML = "";
-
-      const html = `<tr class="new-data">
-      <td class="name">${user.name}</td>
-      <td class="surname">${user.surname}</td>
-      <td class="html point__${
-        user.points[0] >= self.htmlAverage ? "high" : "low"
-      }">${user.points[0]}</td>
-      <td class="css point__${
-        user.points[1] >= self.cssAverage ? "high" : "low"
-      }">${user.points[1]}</td>
-      <td class="javascript point__${
-        user.points[2] >= self.javascriptAverage ? "high" : "low"
-      }">${user.points[2]}</td>
-    </tr>`;
-
-      data.insertAdjacentHTML("beforebegin", html);
+const getSongData = function (artistName) {
+  fetch(`https://itunes.apple.com/search?term=${artistName}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      getSong(data);
     });
-  }
+};
 
-  StudentEventListener() {
-    let self = this;
-    //event listener.
-
-    btn.addEventListener("click", function () {
-      users.push({
-        name: name_inp.value,
-        surname: surn_inp.value,
-        points: [html_inp.value, css_inp.value, js.value],
-      });
-
-      const htmlScores = html_inp.value >= self.htmlAverage ? "high" : "low";
-      const cssScores = css_inp.value >= self.cssAverage ? "high" : "low";
-      const jsScores = js.value >= self.javascriptAverage ? "high" : "low";
-
-      const html = `<tr class="new-data">
-      <td class="name">${name_inp.value}</td>
-      <td class="surname">${surn_inp.value}</td>
-      <td class="html point__${htmlScores}">${html_inp.value}</td>
-      <td class="css point__${cssScores}">${css_inp.value}</td>
-      <td class="javascript point__${jsScores}">${js.value}</td>
-    </tr>`;
-
-      data.insertAdjacentHTML("beforebegin", html);
-
-      //average update.
-
-      //html
-      avgHtml.textContent = (
-        (self.htmlAverage * (users.length - 1) + Number(html_inp.value)) /
-        users.length
-      ).toFixed(2);
-      self.htmlAverage =
-        (self.htmlAverage * (users.length - 1) + Number(html_inp.value)) /
-        users.length;
-
-      //css.
-      avgCss.textContent = (
-        (self.cssAverage * (users.length - 1) + Number(css_inp.value)) /
-        users.length
-      ).toFixed(2);
-      self.cssAverage =
-        (self.cssAverage * (users.length - 1) + Number(css_inp.value)) /
-        users.length;
-
-      //js
-      avgJs.textContent = (
-        (self.javascriptAverage * (users.length - 1) + Number(js.value)) /
-        users.length
-      ).toFixed(2);
-      self.javascriptAverage =
-        (self.javascriptAverage * (users.length - 1) + Number(js.value)) /
-        users.length;
-
-      //input clearing..
-      name_inp.value = "";
-      surname.value = "";
-      css_inp.value = "";
-      html_inp.value = "";
-      js.value = "";
-    });
-  }
-
-  subEventListener() {
-    subBtn.addEventListener("click", function () {
-      subjects.push(subInpt.value);
-
-      let sub = "";
-      sub += `<td>${subInpt.value}</td>`;
-      head.innerHTML += sub;
-
-      subInpt.value = "";
-    });
-  }
-}
-
-new fullTable(users, subjects);
+getSongData('Queen');
+getSongData('The beatles');
