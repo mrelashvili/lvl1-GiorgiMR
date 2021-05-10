4d8345ec6572b18f98e4c2d7c12ea49fcbe838d3
Giorgi Mrelashvili <mrmrela5@gmail.com>
2021-05-10 18:36:57 +0400

    update-v1

--- script.js
+++ script.js
@@ -1,40 +1,23 @@
-'use strict';
+"use strict";
 
-const container = document.querySelector('.display-data');
-const clck = document.querySelector('.clck-btn');
+let firstNum = 1;
+let secondNum = 1;
+let sum = 0;
 
-///////////////////////////////////////
+function fibonnaciEven() {
+  while (firstNum + secondNum < 4000000) {
+    if (firstNum % 2 === 0) {
+      sum += firstNum;
+    }
+    firstNum += secondNum;
+    if (secondNum % 2 === 0) {
+      sum += secondNum;
+    }
+    secondNum += firstNum;
+  }
+  console.log(`Even-valued numbers sum is: ` + sum);
+}
 
-const getSongData = function () {
-  let inpValue = document.querySelector('.inp_txt').value;
+fibonnaciEven();
 
-  fetch(`https://itunes.apple.com/search?term=${inpValue}`)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      song(data);
-    });
-};
-
-const song = function (data) {
-  let html = '';
-  data.results.forEach(results => {
-    html += `<article class="display">
-          <img class="song__img" src="${results.artworkUrl100}" />
-          <div class="song__data">
-            <h3 class="song__name">${results.trackName}</h3>
-            <h4 class="artist__name">${results.artistName}</h4>
-            <p class="row"><span>🆔 </span>${results.artistId}</p>
-            <p class="row"><span>💰</span>${results.collectionPrice} $</p>
-            <p class="row"><span> ⬇</span><a href="${results.trackViewUrl}">Click For More</a></p>
-          </div>
-          <h4 class="preview"><audio controls>
-  <source src="${results.previewUrl}" type="audio/mpeg">
-</audio></h4>
-        </article>`;
-  });
-  container.insertAdjacentHTML('beforeend', html);
-};
-
-clck.addEventListener('click', getSongData);
+//////////////////////////////
