278003d451e4b8aabaea08d408747cffaa4ca6de
Giorgi Mrelashvili <mrmrela5@gmail.com>
2021-05-12 18:32:55 +0400

    update-v1

--- script.js
+++ script.js
@@ -1,23 +1,67 @@
 "use strict";
 
-let firstNum = 1;
-let secondNum = 1;
-let sum = 0;
+const slides = document.querySelectorAll(".slide");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+let curSlide = 0;
+const maxSlide = slides.length;
 
-function fibonnaciEven() {
-  while (firstNum + secondNum < 4000000) {
-    if (firstNum % 2 === 0) {
-      sum += firstNum;
-    }
-    firstNum += secondNum;
-    if (secondNum % 2 === 0) {
-      sum += secondNum;
-    }
-    secondNum += firstNum;
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, index) => (s.style.transform = `translateX(${100 * (index - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+//NExt slide.
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
   }
-  console.log(`Even-valued numbers sum is: ` + sum);
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+//Event listeners..
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
+
+////MODALS.
+
+const modal = document.querySelector(".modal");
+const btnShowModal = document.querySelectorAll(".nav__link");
+const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", openModal);
 }
 
-fibonnaciEven();
+btnCloseModal.addEventListener("click", closeModal);
 
-//////////////////////////////
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") closeModal();
+});
