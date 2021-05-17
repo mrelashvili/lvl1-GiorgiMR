382b74c23065f79e7ec64239e7777a6ed601394b
Giorgi Mrelashvili <mrmrela5@gmail.com>
2021-05-17 18:52:50 +0400

    update-v1

--- script.js
+++ script.js
@@ -38,31 +38,3 @@ const prevSlide = function () {
 
 btnRight.addEventListener("click", nextSlide);
 btnLeft.addEventListener("click", prevSlide);
-
-////MODALS.
-//
-
-const modal = document.querySelector(".modal");
-const btnShowModal = document.querySelectorAll(".nav__link");
-const btnCloseModal = document.querySelector(".close-modal");
-const overlay = document.querySelector(".overlay");
-
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
-
-const openModal = function () {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-};
-
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener("click", openModal);
-}
-
-btnCloseModal.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") closeModal();
-});
