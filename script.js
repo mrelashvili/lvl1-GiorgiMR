e710801d3a9f4772728932e05cb540345096ff99
Giorgi Mrelashvili <mrmrela5@gmail.com>
2021-05-29 18:12:11 +0400

    update-v2

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 "use strict";
+////////////////////////////
 
-const slides = document.querySelectorAll(".slide");
-const btnLeft = document.querySelector(".slider__btn--left");
-const btnRight = document.querySelector(".slider__btn--right");
+const container = document.querySelector(".main__container--bottom");
+const clck = document.querySelector(".vertical-line-four");
+const three = document.querySelector(".vertical-line-three");
 
-let curSlide = 0;
-const maxSlide = slides.length;
+///////////////////////////////////////
 
-const goToSlide = function (slide) {
-  slides.forEach(
-    (s, index) => (s.style.transform = `translateX(${100 * (index - slide)}%)`)
-  );
+const getData = function (amount) {
+  fetch(`https://fakestoreapi.com/products?limit=${amount}`)
+    .then((res) => res.json())
+    .then((data) => info(data));
 };
 
-goToSlide(0);
+const info = function (data) {
+  data.forEach((results) => {
+    console.log(results);
+    container.innerHTML += `
+    <div class='col col-lg-3 col-md-4 col-sm-6'>
+          <div class='card'>
+    <img class="card-img-top image-card" src="${results.image}" alt="Card image cap">
 
-//NExt slide.
-
-const nextSlide = function () {
-  if (curSlide === maxSlide - 1) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
-  goToSlide(curSlide);
+      <div class='card-body'>
+          <div>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+          </div>
+      <div class="product__descr">
+       <p>${results.title}</p>
+      </div>
+      <div class="product__price">
+        <h2>$${results.price}</h2>
+      </div>
+      </div>
+              </div>
+              </div>
+              </div>`;
+  });
 };
 
-const prevSlide = function () {
-  if (curSlide === 0) {
-    curSlide = maxSlide - 1;
-  } else {
-    curSlide--;
-  }
-  goToSlide(curSlide);
-};
-
-//Event listeners..
-
-btnRight.addEventListener("click", nextSlide);
-btnLeft.addEventListener("click", prevSlide);
+clck.addEventListener("click", () => getData(5));
+three.addEventListener("click", () => getData(10));
