a24d91f3c49376d0bd913ea5d705050c98743f0c
Giorgi Mrelashvili <mrmrela5@gmail.com>
2021-05-31 17:45:54 +0400

    update-v3

--- script.js
+++ script.js
@@ -2,22 +2,25 @@
 ////////////////////////////
 
 const container = document.querySelector(".main__container--bottom");
-const clck = document.querySelector(".vertical-line-four");
+const four = document.querySelector(".vertical-line-four");
 const three = document.querySelector(".vertical-line-three");
+const two = document.querySelector(".vertical-line-two");
+const layout = document.querySelector(".fa-table");
+const full_line = document.querySelector(".fa-list-ul");
 
 ///////////////////////////////////////
 
-const getData = function (amount) {
+const getData = function (amount, num) {
   fetch(`https://fakestoreapi.com/products?limit=${amount}`)
     .then((res) => res.json())
-    .then((data) => info(data));
+    .then((data) => info(data, num));
 };
 
-const info = function (data) {
+const info = function (data, num) {
   data.forEach((results) => {
     console.log(results);
     container.innerHTML += `
-    <div class='col col-lg-3 col-md-4 col-sm-6'>
+    <div class='col col-lg-${num} col-md-4 col-sm-6'>
           <div class='card'>
     <img class="card-img-top image-card" src="${results.image}" alt="Card image cap">
 
@@ -29,6 +32,7 @@ const info = function (data) {
         <i class="fas fa-star"></i>
         <i class="far fa-star"></i>
           </div>
+          
       <div class="product__descr">
        <p>${results.title}</p>
       </div>
@@ -36,11 +40,73 @@ const info = function (data) {
         <h2>$${results.price}</h2>
       </div>
       </div>
-              </div>
+
               </div>
               </div>`;
   });
 };
 
-clck.addEventListener("click", () => getData(5));
-three.addEventListener("click", () => getData(10));
+// clck.addEventListener("click", () => {
+//   container.innerHTML = "";
+//   getData(10);
+// });
+
+layout.addEventListener("click", () => {
+  container.innerHTML = "";
+  getData(12);
+});
+
+const info2 = function (data) {
+  data.forEach((results) => {
+    console.log(results);
+    container.innerHTML += `
+    <div class='row'>
+          <div class='card col-lg-2'>
+    <img class="card-img-top image-card full-line-img" src="${results.image}" alt="Card image cap">
+        </div>
+      <div class='card-body col-lg-2 col-md-4'>
+          <div>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="far fa-star"></i>
+          </div>
+          
+      <div class="product__descr col-lg-12 col-md-8">
+       <p>${results.description}</p>
+      </div>
+      <div class="product__price">
+        <h2>$${results.price}</h2>
+      </div>
+      </div>
+
+              </div>
+              </div>`;
+  });
+};
+const gotData = function (amount, num) {
+  fetch(`https://fakestoreapi.com/products?limit=${amount}`)
+    .then((res) => res.json())
+    .then((data) => info2(data, num));
+};
+
+full_line.addEventListener("click", () => {
+  container.innerHTML = "";
+  gotData(12);
+});
+
+three.addEventListener("click", () => {
+  container.innerHTML = "";
+  getData(12, 4);
+});
+
+four.addEventListener("click", () => {
+  container.innerHTML = "";
+  getData(12, 3);
+});
+
+two.addEventListener("click", () => {
+  container.innerHTML = "";
+  getData(12, 6);
+});
